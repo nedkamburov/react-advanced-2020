07921152172af684053e958af70bfa5837fb59af
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2021-03-04 15:26:04 +0100

    lesson 6 without Reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,33 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      setShowModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name: name }]);
+      setName('');
+    } else {
+      setShowModal(true);
+    }
+  }
+
+  return <>
+    {showModal && <Modal />}
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <button type="submit">Add</button>
+    </form>
+    {people.map(person => {
+      return <div key={person.id}>
+        <h4>{person.name}</h4>
+      </div>
+    })}
+  </>
 };
 
 export default Index;
